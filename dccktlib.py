92b6f4b6954e2bd19e4c37ece0ca793ec71540f4
RyanJamesEE805 <65698424+RyanJamesEE805@users.noreply.github.com>
2020-06-11 21:16:59 +0530

    Update dccktlib.py

--- dccktlib.py
+++ dccktlib.py
@@ -1,11 +1,12 @@
-def Finr_vi(x, y):
-    r = x/y
-    return r
+class dclib:
+    def Finr_vi(x, y):
+        r = x/y
+        return r
 
-def Finc_vr(x, y):
-    r = x/y
-    return r
-
-def Finv_ir(x, y):
-    r = x*y
-    return r
+    def Finc_vr(x, y):
+        r = x/y
+        return r
+    
+    def Finv_ir(x, y):
+        r = x*y
+        return r
